5a8f2861e3e33cdf026d9b439fc15622fa3f7a0f
Audrey <audreyleow@yahoo.com.sg>
2022-11-14 23:50:23 +0800

    rewrite routes

--- pages/projects/index.tsx
+++ pages/projects/index.tsx
@@ -1,114 +1,28 @@
-import Link from "next/link";
-import { motion } from "framer-motion";
-import { work_experience } from "./testdata";
+import React from "react";
+import { AnimateSharedLayout, AnimatePresence } from "framer-motion";
+import { Item } from "./[id]";
+import { List } from "./List";
 import { useRouter } from "next/router";
-import styles from "./index.module.css";
-// Our custom easing
-let easing = [0.6, -0.05, 0.01, 0.99];
+import AnimatedWrapper from "./wrapper";
 
-// animate: defines animation
-// initial: defines initial state of animation or stating point.
-// exit: defines animation when component exits
-
-// Custom variant
-const fadeInUp = {
-  initial: {
-    y: 60,
-    opacity: 0,
-    transition: { duration: 0.6, ease: easing },
-  },
-  animate: {
-    y: 0,
-    opacity: 1,
-    transition: {
-      duration: 0.6,
-      ease: easing,
-    },
-  },
-};
-
-const stagger = {
-  animate: {
-    transition: {
-      staggerChildren: 0.1,
-    },
-  },
-};
-
-const MotionLink = motion(Link);
-
-const Index = () => {
+export default function Home() {
   const router = useRouter();
+  const { id }: { id: string } = router.query;
+  const imageHasLoaded = true;
   return (
-    <motion.div initial="initial" animate="animate" exit={{ opacity: 0 }}>
-      <div className={styles["work-container"]}>
-        <motion.div
-          animate={{ opacity: 1 }}
-          initial={{ opacity: 0 }}
-          className={styles["work-title"]}
-        >
-          <div className={styles.heading}>My Work Experiences</div>
-        </motion.div>
-        <motion.div variants={stagger} className={styles.cards}>
-          {work_experience.map((work_experience) => (
-            <div className={styles["item-container"]}>
-              <Link
-                key={work_experience.id}
-                href="/projects/[id]"
-                as={`/projects/${work_experience.id}`}
-              >
-                <motion.div
-                  variants={fadeInUp}
-                  whileHover={{ scale: 1.05 }}
-                  whileTap={{ scale: 0.95 }}
-                  className={styles.item}
-                >
-                  <div className={styles.subheading}>
-                    {work_experience.title}
-                  </div>
-                  <motion.img
-                    initial={{ x: 60, opacity: 0 }}
-                    animate={{ x: 0, opacity: 1 }}
-                    transition={{ delay: 0.2 }}
-                    key={work_experience.images[0]}
-                    src={work_experience.images[0]}
-                    width={250}
-                    height={150}
-                    style={{ marginTop: "20px", marginBottom: "20px" }}
-                  />
-                  <div className={styles.subheading}>
-                    <div>{work_experience.role}</div>
-                  </div>
-                  <div className={styles.date}>
-                    {`${work_experience.start} - ${work_experience.end}`}
-                  </div>
-                </motion.div>
-              </Link>
-            </div>
-          ))}
-        </motion.div>
-        <motion.div style={{ paddingTop: "20px" }}>
-          <MotionLink
-            href="/"
-            variants={fadeInUp}
-            className={styles["go-back"]}
-          >
-            ← Back to main page
-          </MotionLink>
-        </motion.div>
-      </div>
-    </motion.div>
+    <>
+      <AnimatedWrapper>
+        {id && imageHasLoaded && <Item id={id} key="item" />}
+      </AnimatedWrapper>
+    </>
   );
-};
+}
 
-// Index.getInitialProps = async function() {
-//   const res = await fetch(
-//     "https://my-json-server.typicode.com/wrongakram/demo/projects"
-//   );
-//   const data = await res.json();
-//   return {
-//     products: data
+// export const getStaticProps: GetStaticProps = async () => {
+//     const allWorkData = getSortedWorkData();
+//     return {
+//       props: {
+//         allWorkData,
+//       },
+//     };
 //   };
-// };
-
-export default Index;
--- pages/work_experience/[id].tsx
+++ pages/work_experience/[id].tsx
@@ -1,81 +1,131 @@
-import React, { useState, useEffect } from "react";
+import React from "react";
 import { motion } from "framer-motion";
 import Link from "next/link";
-import { items } from "./data";
-import Image from "next/image";
-import testimage from "../../public/nodeflair1.png";
-import AnimatedWrapper from "./wrapper";
+import { work_experience } from "./testdata";
+import { GetStaticPaths, GetStaticProps } from "next/types";
+import CarouselSwiper from "../../components/CarouselSwiper";
+import styles from "./index.module.css";
 
-export default function Item({ id }: { id: string }) {
-  const [category, setCategory] = useState("");
-  const [title, setTitle] = useState("");
-  useEffect(() => {
-    const item = items.find((item) => item.id === id);
-    if (item) {
-      setCategory(item.role);
-      setTitle(item.title);
-    }
-  }, [items]);
+let easing = [0.6, -0.05, 0.01, 0.99];
+
+const stagger = {
+  animate: {
+    transition: {
+      staggerChildren: 0.05,
+    },
+  },
+};
+
+const fadeInUp = {
+  initial: {
+    y: 60,
+    opacity: 0,
+    transition: { duration: 0.6, ease: easing },
+  },
+  animate: {
+    y: 0,
+    opacity: 1,
+    transition: {
+      duration: 0.6,
+      ease: easing,
+    },
+  },
+};
+
+const MotionLink = motion(Link);
+
+const Project = ({
+  indivWork,
+}: {
+  indivWork: {
+    id: string;
+    role: string;
+    title: string;
+    images: string[];
+    details: string[];
+    tech_stack: string;
+    start: string;
+    end: string;
+  };
+}) => {
+  const workDetails = () =>
+    indivWork.details.map((line: string) => {
+      return (
+        <motion.li
+          style={{ listStyle: "disc" }}
+          initial={{ opacity: 0, scale: 0.5 }}
+          animate={{ opacity: 1, scale: 1 }}
+          transition={{
+            duration: 0.8,
+            delay: 0.5,
+            ease: [0, 0.71, 0.2, 1.01],
+          }}
+        >
+          {line}
+        </motion.li>
+      );
+    });
 
   return (
-    <AnimatedWrapper>
+    <div className={styles["work-details-container"]}>
       <motion.div
-        initial={{ opacity: 0 }}
-        animate={{ opacity: 1 }}
-        exit={{ opacity: 0, transition: { duration: 0.15 } }}
-        transition={{ duration: 0.2, delay: 0.15 }}
-        style={{ pointerEvents: "auto" }}
-        className="overlay"
+        initial="hidden"
+        animate="enter"
+        variants={{
+          hidden: { opacity: 0, x: -200, y: 0 },
+          enter: { opacity: 1, x: 0, y: 0, transition: { duration: 1 } },
+          exit: { opacity: 0, transition: { duration: 1 } },
+        }}
+        transition={{ type: "linear" }}
+        style={{ padding: "30px 20px 20px" }}
       >
-        <Link href="/work_experience" />
-      </motion.div>
-      <div className="card-content-container open">
-        <motion.div className="card-content" layoutId={`card-container-${id}`}>
-          <motion.div
-            className="card-image-container"
-            layoutId={`card-image-container-${id}`}
-          >
-            <Image className="card-image" src={testimage} alt="test-image" />
+        <motion.div variants={stagger}>
+          <motion.div className={styles.subheading} variants={fadeInUp}>
+            {indivWork.title}
           </motion.div>
-          <motion.div
-            className="title-container"
-            layoutId={`title-container-${id}`}
-          >
-            <span className="category">{category}</span>
-            <h2>{title}</h2>
+          <motion.div className={styles.heading} variants={fadeInUp}>
+            {indivWork.role}
           </motion.div>
-          <motion.div className="content-container" animate>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
-            ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-            aliquip ex ea commodo consequat. Duis aute irure dolor in
-            reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
-            pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
-            culpa qui officia deserunt mollit anim id est laborum. Lorem ipsum
-            dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor
-            incididunt ut labore et dolore magna aliqua. Ut enim ad minim
-            veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex
-            ea commodo consequat. Duis aute irure dolor in reprehenderit in
-            voluptate velit esse cillum dolore eu fugiat nulla pariatur.
-            Excepteur sint occaecat cupidatat non proident, sunt in culpa qui
-            officia deserunt mollit anim id est laborum. Lorem ipsum dolor sit
-            amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt
-            ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis
-            nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
-            consequat. Duis aute irure dolor in reprehenderit in voluptate velit
-            esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat
-            cupidatat non proident, sunt in culpa qui officia deserunt mollit
-            anim id est laborum. Lorem ipsum dolor sit amet, consectetur
-            adipiscing elit, sed do eiusmod tempor incididunt ut labore et
-            dolore magna aliqua. Ut enim ad minim veniam, quis nostrud
-            exercitation ullamco laboris nisi ut aliquip ex ea commodo
-            consequat. Duis aute irure dolor in reprehenderit in voluptate velit
-            esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat
-            cupidatat non proident, sunt in culpa qui officia deserunt mollit
-            anim id est laborum.
+          <motion.div variants={fadeInUp}>
+            <motion.ol className={styles.details}>{workDetails()}</motion.ol>
+          </motion.div>
+          <motion.div>
+            <MotionLink
+              href="/projects"
+              variants={fadeInUp}
+              className={styles["go-back"]}
+            >
+              ← Back to my other work experiences
+            </MotionLink>
           </motion.div>
         </motion.div>
+      </motion.div>
+      <div className={styles["carousel-swiper"]}>
+        <CarouselSwiper images={indivWork.images} />
       </div>
-    </AnimatedWrapper>
+    </div>
   );
-}
+};
+
+export default Project;
+
+export const getStaticPaths: GetStaticPaths = async () => {
+  const paths = work_experience.map((indivWork) => ({
+    params: { id: indivWork.id },
+  }));
+  return {
+    paths,
+    fallback: false,
+  };
+};
+
+export const getStaticProps: GetStaticProps = async ({ params }) => {
+  const indivWork = work_experience.find(
+    (indivWork) => indivWork.id === params?.id
+  );
+  return {
+    props: {
+      indivWork,
+    },
+  };
+};
