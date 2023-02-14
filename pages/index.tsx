c39127843cc226ec75c54e118a7359b955a311b8
Audrey <audreyleow@yahoo.com.sg>
2023-02-14 12:45:20 +0800

    social sharing

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,12 @@
 import Head from "next/head";
+import Image from "next/image";
+import Link from "next/link";
 import Layout from "../components/layout";
 import styles from "../components/layout.module.css";
+import LinkedInIcon from "../components/icons/LinkedInIcon";
+import AIcon from "../public/icons/a-icon.png";
+import GithubIcon from "../public/icons/github-logo.png";
+import EmailIcon from "../public/icons/email-logo.png";
 // import { getSortedPostsData } from "../lib/posts";
 
 const name = "[Audrey Leow]";
@@ -30,6 +36,28 @@ export default function Home({
               I'm a full-stack software engineer that mainly works with React.js
               and Ruby on Rails but I am language agnostic and interested in
               designing frontend interfaces and architecting backend processes.
+            </div>
+            <div className={styles["indiv-links"]}>
+              <Link
+                href={"https://github.com/audreyleow"}
+                className={styles["a-icon"]}
+              >
+                <Image src={GithubIcon} alt="a icon" width={32} height={32} />
+              </Link>
+              <Link
+                href={"http://linkedin.com/in/audrey-leow"}
+                className={styles.icon}
+              >
+                <LinkedInIcon />
+              </Link>
+              <Link href={"mailto:audreyleow@yahoo.com.sg"}>
+                <div className={styles["email-icon"]}>
+                  <Image src={EmailIcon} alt="a icon" width={38} height={38} />
+                </div>
+              </Link>
+              {/* <div className={styles["a-icon"]}>
+                <Image src={AIcon} alt="a icon" width={36} height={36} />
+              </div> */}
             </div>
           </div>
           <div className={styles.pic}>
